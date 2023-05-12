d640b488d95a96609e98fbcc73f015f14a38ca53
maravilla2610 <127360395+maravilla2610@users.noreply.github.com>
2023-05-11 19:32:49 -0600

    Update app.py

--- app.py
+++ app.py
@@ -7,9 +7,22 @@ from google.cloud import bigquery
 import requests 
 import json 
 import os
+import base64
+import tempfile 
 
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "/Users/vize/Desktop/Semestre_8/myenv/Colmena_Start/colmena-386419-8c8c1d805f37.json"
 
+encoded_key = os.environ.get("GOOGLE_APPLICATION_CREDENTIALS")
+
+# Decode the key
+key = json.loads(base64.b64decode(encoded_key).decode())
+
+# Write the key to a temporary file
+with tempfile.NamedTemporaryFile(mode="w", delete=False) as temp_key_file:
+    json.dump(key, temp_key_file)
+    temp_key_file_path = temp_key_file.name
+
+# Set the environment variable to the path of the temporary file
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = temp_key_file_path
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///registros.db"
